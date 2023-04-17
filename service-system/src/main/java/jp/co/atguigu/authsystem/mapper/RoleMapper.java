39ea41e37894c3598081313907b932e257f4cfe9
ArcaHozota <arkamahozota@gmail.com>
2023-04-17 18:13:53 +0900

    1.05

--- service-system/src/main/java/jp/co/atguigu/authsystem/mapper/RoleMapper.java
+++ service-system/src/main/java/jp/co/atguigu/authsystem/mapper/RoleMapper.java
@@ -1,6 +1,10 @@
 package jp.co.atguigu.authsystem.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import jp.co.atguigu.model.entity.Role;
 
 /**
  * ロール授権のマッパーインターフェス
@@ -9,4 +13,11 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface RoleMapper {
+
+	/**
+	 * すべてのデータを吹き出します
+	 *
+	 * @return List<Role>
+	 */
+	List<Role> findAll();
 }
