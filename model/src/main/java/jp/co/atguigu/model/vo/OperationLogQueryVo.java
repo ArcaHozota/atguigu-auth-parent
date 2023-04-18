029a5cd7d7bd0190ab99b28174175aca97cbeabd
ArcaHozota <arkamahozota@gmail.com>
2023-04-18 14:21:13 +0900

    1.16

--- model/src/main/java/jp/co/atguigu/model/vo/OperationLogQueryVo.java
+++ model/src/main/java/jp/co/atguigu/model/vo/OperationLogQueryVo.java
@@ -2,10 +2,31 @@ package jp.co.atguigu.model.vo;
 
 import lombok.Data;
 
+/**
+ * 操作ログのクエリVO
+ *
+ * @author Administrator
+ */
 @Data
 public class OperationLogQueryVo {
+
+	/**
+	 * 標題
+	 */
 	private String title;
-	private String operName;
+
+	/**
+	 * 操作名稱
+	 */
+	private String operationName;
+
+	/**
+	 * 創建時間(開始)
+	 */
 	private String createTimeBegin;
+
+	/**
+	 * 創建時間(結束)
+	 */
 	private String createTimeEnd;
 }
