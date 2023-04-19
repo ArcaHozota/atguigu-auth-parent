cd78c762c984c7d6b3749ed6a2147e0fd19c7f6c
ArcaHozota <arkamahozota@gmail.com>
2023-04-19 10:31:01 +0900

    1.27

--- common/common-util/src/main/java/jp/co/atguigu/common/utils/Pagination.java
+++ common/common-util/src/main/java/jp/co/atguigu/common/utils/Pagination.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 import java.util.List;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 /**
@@ -15,7 +14,6 @@ import lombok.Setter;
  */
 @Getter
 @Setter
-@NoArgsConstructor
 public final class Pagination<T> {
 
 	/**
