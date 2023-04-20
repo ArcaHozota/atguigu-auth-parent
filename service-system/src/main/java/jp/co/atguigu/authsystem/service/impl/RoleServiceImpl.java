03fd61f9a06e9a73019f38d436fe6d4f49f520d5
ArcaHozota <arkamahozota@gmail.com>
2023-04-20 21:40:08 +0900

    1.62

---
+++ service-system/src/main/java/jp/co/atguigu/authsystem/service/impl/RoleServiceImpl.java
@@ -0,0 +1,36 @@
+package jp.co.atguigu.authsystem.service.impl;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import jp.co.atguigu.authsystem.mapper.RoleMapper;
+import jp.co.atguigu.authsystem.service.RoleService;
+import jp.co.atguigu.model.entity.Role;
+
+/**
+ * ロール情報の実装クラス
+ *
+ * @author Administrator
+ */
+@Service
+public class RoleServiceImpl implements RoleService {
+
+	/**
+	 * ロールマッパー
+	 */
+	@Resource
+	private RoleMapper roleMapper;
+
+	/**
+	 * すべてのロール情報を取得する
+	 *
+	 * @return List<Role>
+	 */
+	@Override
+	public List<Role> getAll() {
+		return this.roleMapper.findAll();
+	}
+}
