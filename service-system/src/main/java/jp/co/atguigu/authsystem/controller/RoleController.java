434450174105b17b21e3993d8bac4e32d8ff1b7e
ArcaHozota <arkamahozota@gmail.com>
2023-04-20 21:44:28 +0900

    1.65

--- service-system/src/main/java/jp/co/atguigu/authsystem/controller/RoleController.java
+++ service-system/src/main/java/jp/co/atguigu/authsystem/controller/RoleController.java
@@ -5,10 +5,19 @@ import java.util.List;
 import javax.annotation.Resource;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import jp.co.atguigu.authsystem.service.RoleService;
 import jp.co.atguigu.model.entity.Role;
 
+/**
+ * ロールコントローラー
+ *
+ * @author Administrator
+ */
+@RestController
+@RequestMapping("/admin/system/sysRole")
 public class RoleController {
 
 	/**
