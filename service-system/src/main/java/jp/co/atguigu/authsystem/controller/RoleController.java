37fe64174d77b31f74d9a9f142ed8838c10ecb62
ArcaHozota <arkamahozota@gmail.com>
2023-04-25 13:42:49 +0900

    1.70

--- service-system/src/main/java/jp/co/atguigu/authsystem/controller/RoleController.java
+++ service-system/src/main/java/jp/co/atguigu/authsystem/controller/RoleController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import jp.co.atguigu.authsystem.service.RoleService;
 import jp.co.atguigu.model.entity.Role;
 
@@ -18,6 +20,7 @@ import jp.co.atguigu.model.entity.Role;
  *
  * @author Administrator
  */
+@Api(tags = "ロール授権処理API")
 @RestController
 @RequestMapping("/admin/system/sysRole")
 public class RoleController {
@@ -33,6 +36,7 @@ public class RoleController {
 	 *
 	 * @return List<Role>
 	 */
+	@ApiOperation("全件検索")
 	@GetMapping("/findAll")
 	public List<Role> getAllRoles() {
 		return this.roleService.getAll();
@@ -43,6 +47,7 @@ public class RoleController {
 	 *
 	 * @param id ロールID
 	 */
+	@ApiOperation("論理削除")
 	@DeleteMapping("/remove/{id}")
 	public void removeRole(@PathVariable final Long id) {
 		this.roleService.remove(id);
