9d97cefba00e71b4cef163c403473df91dc5c828
ArcaHozota <arkamahozota@gmail.com>
2023-04-26 19:03:52 +0900

    1.74

--- model/src/main/java/jp/co/atguigu/model/entity/BasicEntity.java
+++ model/src/main/java/jp/co/atguigu/model/entity/BasicEntity.java
@@ -3,6 +3,7 @@ package jp.co.atguigu.model.entity;
 import java.io.Serializable;
 import java.sql.Timestamp;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
@@ -18,20 +19,24 @@ public class BasicEntity implements Serializable {
 	/**
 	 * ID
 	 */
+	@ApiModelProperty(value = "ID")
 	private Long id;
 
 	/**
 	 * 作成時間
 	 */
+	@ApiModelProperty(value = "創建時間")
 	private Timestamp creationTime;
 
 	/**
 	 * 更新時間
 	 */
+	@ApiModelProperty(value = "更新時間")
 	private Timestamp updatingTime;
 
 	/**
 	 * 論理削除フラグ
 	 */
+	@ApiModelProperty(value = "邏輯刪除標志")
 	private String logicDeleteFlg;
 }
