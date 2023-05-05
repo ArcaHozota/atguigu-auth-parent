33753b87b1ce330ba09a17f1d53b09272faf9c86
ArcaHozota <arkamahozota@gmail.com>
2023-05-05 13:00:03 +0900

    1.76

---
+++ common/common-util/src/main/java/jp/co/atguigu/common/utils/Result.java
@@ -0,0 +1,106 @@
+package jp.co.atguigu.common.utils;
+
+import lombok.Data;
+
+/**
+ * データ返却クラス
+ *
+ * @author Administrator
+ * @param <T> ジェネリック
+ */
+@Data
+public class Result<T> {
+
+	/**
+	 * ステータスコード
+	 */
+	private Integer code;
+
+	/**
+	 * メッセージ
+	 */
+	private String message;
+
+	/**
+	 * データ
+	 */
+	private T data;
+
+	/**
+	 * データを返却する
+	 *
+	 * @param <T>  ジェネリック
+	 * @param data データ
+	 * @return Result<T>
+	 */
+	protected static <T> Result<T> build(final T data) {
+		final Result<T> result = new Result<>();
+		if (data != null) {
+			result.setData(data);
+		}
+		return result;
+	}
+
+	/**
+	 * 結果を返却する
+	 *
+	 * @param <T>     ジェネリック
+	 * @param body    データ
+	 * @param code    ステータス
+	 * @param message メッセージ
+	 * @return Result<T>
+	 */
+	public static <T> Result<T> build(final T body, final Integer code, final String message) {
+		final Result<T> result = build(body);
+		result.setCode(code);
+		result.setMessage(message);
+		return result;
+	}
+
+	public static <T> Result<T> build(final T body, final ResultCodeEnum resultCodeEnum) {
+		final Result<T> result = build(body);
+		result.setCode(resultCodeEnum.getCode());
+		result.setMessage(resultCodeEnum.getMessage());
+		return result;
+	}
+
+	public static <T> Result<T> ok() {
+		return Result.ok(null);
+	}
+
+	/**
+	 * 操作成功
+	 *
+	 * @param data baseCategory1List
+	 * @param <T>
+	 * @return
+	 */
+	public static <T> Result<T> ok(final T data) {
+		return build(data, ResultCodeEnum.SUCCESS);
+	}
+
+	public static <T> Result<T> fail() {
+		return Result.fail(null);
+	}
+
+	/**
+	 * 操作失败
+	 *
+	 * @param data
+	 * @param <T>
+	 * @return
+	 */
+	public static <T> Result<T> fail(final T data) {
+		return build(data, ResultCodeEnum.FAILURE);
+	}
+
+	public Result<T> message(final String msg) {
+		this.setMessage(msg);
+		return this;
+	}
+
+	public Result<T> code(final Integer code) {
+		this.setCode(code);
+		return this;
+	}
+}
---
+++ common/common-util/src/main/java/jp/co/atguigu/common/utils/ResultCodeEnum.java
@@ -0,0 +1,103 @@
+package jp.co.atguigu.common.utils;
+
+import lombok.Getter;
+
+/**
+ * データ返却列挙型クラス
+ *
+ * @author Administrator
+ */
+@Getter
+public enum ResultCodeEnum {
+
+	/**
+	 * 成功
+	 */
+	SUCCESS(200, "成功"),
+
+	/**
+	 * 失敗
+	 */
+	FAILURE(201, "失敗"),
+
+	/**
+	 * サービスエラー
+	 */
+	SERVICE_ERROR(2012, "サービスエラー"),
+
+	/**
+	 * データエラー
+	 */
+	DATA_ERROR(204, "データエラー"),
+
+	/**
+	 * 違法的なリクエスト
+	 */
+	ILLEGAL_REQUEST(205, "違法的なリクエスト"),
+
+	/**
+	 * 重複的な送信
+	 */
+	REPEAT_SUBMIT(206, "重複的な送信"),
+
+	/**
+	 * 引数の検証エラー
+	 */
+	ARGUMENT_VALID_ERROR(210, "引数の検証エラー"),
+
+	/**
+	 * 登録しません
+	 */
+	LOGIN_AUTH(208, "登録しません"),
+
+	/**
+	 * 権限はなし
+	 */
+	PERMISSION(209, "権限はなし"),
+
+	/**
+	 * アカウントエラー
+	 */
+	ACCOUNT_ERROR(214, "アカウントエラー"),
+
+	/**
+	 * パスワードエラー
+	 */
+	PASSWORD_ERROR(215, "パスワードエラー"),
+
+	/**
+	 * 登録できません
+	 */
+	LOGIN_MOBILE_ERROR(216, "登録できません"),
+
+	/**
+	 * アカウントは廃止された
+	 */
+	ACCOUNT_STOP(217, "アカウントは廃止された"),
+
+	/**
+	 * ノードエラー
+	 */
+	NODE_ERROR(218, "ノードエラー");
+
+	/**
+	 * ステータスコード
+	 */
+	private Integer code;
+
+	/**
+	 * メッセージ
+	 */
+	private String message;
+
+	/**
+	 * コンストラクタ
+	 *
+	 * @param code    ステータスコード
+	 * @param message メッセージ
+	 */
+	private ResultCodeEnum(final Integer code, final String message) {
+		this.code = code;
+		this.message = message;
+	}
+}
