afa6dad2b4a066eb02bedf2c6063c0ec0e3e6304
ArcaHozota <arkamahozota@gmail.com>
2023-07-12 16:25:50 +0900

    1.95

--- common/common-util/src/main/java/jp/co/atguigu/common/utils/BasicContextUtils.java
+++ common/common-util/src/main/java/jp/co/atguigu/common/utils/BasicContextUtils.java
@@ -4,6 +4,7 @@ import java.sql.Timestamp;
 
 import jp.co.atguigu.common.constants.Constants;
 import jp.co.atguigu.model.entity.BasicEntity;
+import org.springframework.lang.NonNull;
 
 /**
  * 共通更新及び保存処理クラス
@@ -27,7 +28,7 @@ public class BasicContextUtils {
 	 *
 	 * @param aEntity 共通エンティティ
 	 */
-	public static void fillWithInsert(final BasicEntity aEntity) {
+	public static void fillWithInsert(@NonNull final BasicEntity aEntity) {
 		aEntity.setId(SnowflakeUtils.nextId());
 		aEntity.setCreationTime(CURRENT_TIME);
 		aEntity.setUpdatingTime(CURRENT_TIME);
@@ -39,7 +40,7 @@ public class BasicContextUtils {
 	 *
 	 * @param aEntity 共通エンティティ
 	 */
-	public static void fillWithUpdate(final BasicEntity aEntity) {
+	public static void fillWithUpdate(@NonNull final BasicEntity aEntity) {
 		aEntity.setUpdatingTime(CURRENT_TIME);
 	}
 }
